f8ca0e889e3d31432b687c69f4438d9e241a898d
benjnp <benjnp@gmail.com>
2024-02-18 15:15:22 +0800

    Added Admin Orders functionality

--- src/app/(admin)/orders/_layout.tsx
+++ src/app/(admin)/orders/_layout.tsx
@@ -1,5 +1,10 @@
 import {  Stack } from 'expo-router';
 
-export default function AuthLayout() {
-  return <Stack />;
+export default function MenuStack() {
+  return (
+    <Stack>
+      {/* <Stack.Screen name="index" options={{title: 'Orders'}} /> */}
+      <Stack.Screen name="list" options={{headerShown: false}} />
+    </Stack>
+  )
 };
--- src/app/(admin)/orders/list/index.tsx
+++ src/app/(admin)/orders/list/index.tsx
@@ -1,13 +1,13 @@
 // import { FlatList } from 'react-native-gesture-handler';
 import { FlatList } from 'react-native';
-import orders from '../../../../assets/data/orders';
-import OrderListItem from '../../../components/OrderListItem';
+import orders from '@/assets/data/orders';
+import OrderListItem from '@/src/components/OrderListItem';
 import { Stack } from 'expo-router';
 
 export default function OrdersScreen() {
   return (
     <>
-      <Stack.Screen options={{ title: 'Orders' }} />
+      {/* <Stack.Screen options={{ title: 'Orders' }} /> */}
       <FlatList
         data={orders}
         contentContainerStyle={{ gap: 10, padding: 10 }}
